1c97ab6d98e50f04b02ce6f9eb46aeaa971d84a2
Burak Tagtekin <burak.tagtekin@icloud.com>
2024-02-14 02:37:03 +0300

    Remove root. Change epoch.

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ model = YOLO("yolov8n.yaml").load("yolov8n.pt")  # build from YAML and transfer 
 # Train the model
 results = model.train(
     data="coco128.yaml",
-    epochs=100,
+    epochs=2,
     imgsz=640,
     project="mlops",
     name="IE-1001",
