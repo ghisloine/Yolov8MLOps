a6bcca54720b532283ba7986ed4991aff2ebc47b
Burak Tagtekin <burak.tagtekin@icloud.com>
2024-02-15 20:52:50 +0300

    Add folder create method.

--- val.py
+++ val.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO
 import json
+import os
 
 # Load a model
 model = YOLO("yolov8n.pt")  # load an official model
@@ -16,6 +17,12 @@ print(metrics.box.map50)  # map50
 print(metrics.box.map75)  # map75
 print(metrics.box.maps)  # a list contains map50-95 of each category
 
+folder_path = "myresults"
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+    print(f"Folder '{folder_path}' created.")
+else:
+    print(f"Folder '{folder_path}' already exists.")
 
 with open("myresults/metrics.json", "w+") as fp:
     data = {
